d715f1c98144cc7fbd2706f6304a26f1d40bfa2b
vitalves <vitalvesilva@gmail.com>
2019-11-01 16:57:20 -0300

    Página de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input type="text" name="name" id="name" placeholder="Nome completo" />
+        <Input type="email" name="email" id="email" placeholder="Seu email" />
+
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassword"
+          id="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Sua nova senha"
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          placeholder="Confirme sua nova senha"
+        />
+
+        <button type="submit"> Atualizar perfil </button>
+      </Form>
+
+      <button type="button"> Sair do GoBarber </button>
+    </Container>
+  );
 }
